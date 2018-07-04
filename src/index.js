9cfd3c5a790173db3a8decac830ac4d602208add
wasifmiraz <wasifmiraz@mail.com>
2018-07-04 21:42:15 +0600

    src file indexjs setup selector

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import './index.css'
+
+window.onload = function() {
+
+    let taskField = document.querySelector('#taskField')
+    let taskBtn= document.querySelector('#addTaskBtn')
+    let allTasksParent = document.querySelector('#allTasks')
+
+   
+}
+
