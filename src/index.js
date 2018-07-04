95dd2141c5f7ecdf58851b553d0935b4459c9aa6
wasifmiraz <wasifmiraz@mail.com>
2018-07-04 22:41:33 +0600

    audiotraclistwindow function create

--- src/index.js
+++ src/index.js
@@ -19,5 +19,32 @@ window.onload = function() {
 }
 
 function createNewTask(parent, task) {
+console.log(task)
+}
 
+
+AudioTrackListwindow.create = function () {
+
+  if (arguments.length === 0) {
+      return document.createElement('div');
+  }
+
+  if (arguments.length === 1 && typeof arguments[0] != 'object') {
+      return document.createElement(arguments[0]);
+  }
+
+  var tag = arguments[0];
+  var attr = arguments[1] || arguments[0];
+
+  if (arguments.length === 1 && typeof arguments[0] === 'object') {
+      tag = 'div';
+  }
+
+  var element = document.createElement(tag);
+
+  for (var i in attr) {
+      element.setAttribute(i, attr[i]);
+  }
+
+  return element;
 }
